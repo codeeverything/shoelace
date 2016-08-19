ef7368311acc5ce61cc2184f3af4a724a8fd2b37
codeeverything <mike@localitysolutions.co.uk>
2016-08-19 09:51:35 +0100

    Cleanup

--- shoelace.sh
+++
@@ -1,7 +0,0 @@
-#!
-
-git remote add -f shoelace https://github.com/codeeverything/shoelace.git
-git merge -s ours --no-commit shoelace/master
-git read-tree --prefix=.shoelace -u shoelace/master
-git commit -m "Merge Shoelace into .shoelace"
-git remote remove shoelace
